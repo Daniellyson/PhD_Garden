e6325d39e357017352b302d171eecb6596f38b5c
Daniellyson <etu32290@henallux.be>
2019-10-07 18:10:31 +0200

    first commit

---
+++ src/main/java/com/spring/henallux/phD_Garden/PhDGardenApplication.java
@@ -0,0 +1,13 @@
+package com.spring.henallux.phD_Garden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhDGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhDGardenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/henallux/phD_Garden/PhDGardenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.henallux.phD_Garden;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhDGardenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
