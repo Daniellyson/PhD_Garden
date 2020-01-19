0c7f1bdd517452491d797a4d17663169d15e76f3
Daniellyson <etu32290@henallux.be>
2020-01-19 10:36:47 +0100

    images, db dml

--- data_base/phd_garden_dml.sql
+++ data_base/phd_garden_dml.sql
@@ -33,26 +33,33 @@ INSERT INTO translation_category VALUES (4, 2, 'Outils');
 
 /*PRODUCTS*/
 
-INSERT INTO product (category_id, price, stock) VALUES (2, 1.9, 30);
+INSERT INTO product (category_id, price, stock, url_image) VALUES (2, 1.9, 30, "tomato");
 
-INSERT INTO product (category_id, price, stock) VALUES (4, 20.83, 20);
+INSERT INTO product (category_id, price, stock, url_image) VALUES (4, 20.83, 20, "shovel");
 
-INSERT INTO product (category_id, price, stock) VALUES (2, 1.25, 50);
-INSERT INTO product (category_id, price, stock) VALUES (2, 0.42, 30);
-INSERT INTO product (category_id, price, stock) VALUES (2, 0.62, 25);
+INSERT INTO product (category_id, price, stock, url_image) VALUES (2, 1.25, 50, "tomato_seeds");
 
-INSERT INTO product (category_id, price, stock, url_image) VALUES (1, 8.5, 15, 'rose');
-INSERT INTO product (category_id, price, stock, url_image) VALUES (1, 4, 40, 'clove-pink');
+INSERT INTO product (category_id, price, stock, url_image) VALUES (2, 0.42, 30, "zucchini");
 
-INSERT INTO product (category_id, price, stock) VALUES (3, 15.25, 15);
-INSERT INTO product (category_id, price, stock) VALUES (3, 21, 7);
-INSERT INTO product (category_id, price, stock) VALUES (3, 13.75, 10);
+INSERT INTO product (category_id, price, stock, url_image) VALUES (2, 0.62, 25, "carrot");
+
+INSERT INTO product (category_id, price, stock, url_image) VALUES (1, 8.5, 15, "rose");
+
+INSERT INTO product (category_id, price, stock, url_image) VALUES (1, 4, 40, "clove-pink");
+
+INSERT INTO product (category_id, price, stock, url_image) VALUES (3, 15.25, 15, "apple_tree");
+
+INSERT INTO product (category_id, price, stock, url_image) VALUES (3, 21, 7, "cerasus");
+
+INSERT INTO product (category_id, price, stock, url_image) VALUES (3, 13.75, 10, "plum_tree");
+
+INSERT INTO product (category_id, price, stock, url_image) VALUES (4, 3.6, 20, "work_gloves");
+
+INSERT INTO product (category_id, price, stock, url_image) VALUES (4, 12, 30, "rake");
+
+INSERT INTO product (category_id, price, stock, url_image) VALUES (1, 7.75, 12, "dragon-flower");
 
 
-INSERT INTO product (category_id, price, stock) VALUES (4, 3.6, 20);
-INSERT INTO product (category_id, price, stock) VALUES (4, 12, 30);
-
-INSERT INTO product (category_id, price, stock, url_image) VALUES (1, 7.75, 12, 'dragon-flower');
 
 /*TRANSLATION_PRODUCT*/
 
@@ -64,25 +71,31 @@ INSERT INTO  translation_product VALUES (2, 1, 'Shovel', 'A resistant shovel to 
 
 INSERT INTO  translation_product VALUES (3, 2, 'Graines de tomates', 'Un sachet de graines de tomates');
 INSERT INTO  translation_product VALUES (3, 1, 'Tomato seeds', 'A bag of tomato seeds');
+
 INSERT INTO  translation_product VALUES (4, 2, 'Courgette', 'Cultivées en Wallonie');
-INSERT INTO  translation_product VALUES (4, 1, 'Courgette', 'Cultivated in Wallonia');
+INSERT INTO  translation_product VALUES (4, 1, 'Zucchini', 'Cultivated in Wallonia');
+
 INSERT INTO  translation_product VALUES (5, 2, 'Carotte', 'Cultivées en Wallonie');
 INSERT INTO  translation_product VALUES (5, 1, 'Carrot', 'Cultivated in Wallonia');
 
 INSERT INTO  translation_product VALUES (6, 2, 'Rose', 'Petite rose de coulor orange');
 INSERT INTO  translation_product VALUES (6, 1, 'Rose', 'Small rose, orange color');
+
 INSERT INTO  translation_product VALUES (7, 2, 'Oeillet', 'Grande fleur coulor rouge');
 INSERT INTO  translation_product VALUES (7, 1, 'Clove pink', 'Big flower, red color');
 
 INSERT INTO  translation_product VALUES (8, 2, 'Pommier', 'Cultivées en Wallonie');
 INSERT INTO  translation_product VALUES (8, 1, 'Apple tree', 'Cultivated in Wallonia');
+
 INSERT INTO  translation_product VALUES (9, 2, 'Cerisier', 'Cultivés en serre en Wallonie');
 INSERT INTO  translation_product VALUES (9, 1, 'Cerasus ', 'Grown in a greenhouse in Wallonia');
+
 INSERT INTO  translation_product VALUES (10, 2, 'Prunier', 'Cultivées en Wallonie');
 INSERT INTO  translation_product VALUES (10, 1, 'Plum tree', 'Cultivated in Wallonia');
 
 INSERT INTO  translation_product VALUES (11, 2, 'Gants de travail', 'Deux paires de gants de protection');
 INSERT INTO  translation_product VALUES (11, 1, 'Work gloves', 'Two pairs of protective gloves');
+
 INSERT INTO  translation_product VALUES (12, 2, 'Rateau', 'Un rateau pour préparer votre terrain');
 INSERT INTO  translation_product VALUES (12, 1, 'Rake', 'A rake to prepare your land');
 
